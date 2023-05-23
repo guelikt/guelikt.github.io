4ddc0df3aa1bdf103a6550a435db3671a6c2d3cf
guelikt <105388182+guelikt@users.noreply.github.com>
2023-05-23 15:13:27 +0200

    Add files via upload

---
+++ ecar/script.js
@@ -0,0 +1,50 @@
+function projet1() {
+    document.getElementById("projet1").style.display = "block";
+}
+  
+function closeProjet1() {
+    document.getElementById("projet1").style.display = "none";
+}
+function projet2() {
+    document.getElementById("projet2").style.display = "block";
+}
+  
+function closeProjet2() {
+    document.getElementById("projet2").style.display = "none";
+}
+function projet3() {
+    document.getElementById("projet3").style.display = "block";
+}
+  
+function closeProjet3() {
+    document.getElementById("projet3").style.display = "none";
+}
+function projet4() {
+    document.getElementById("projet4").style.display = "block";
+}
+  
+function closeProjet4() {
+    document.getElementById("projet4").style.display = "none";
+}
+function projet5() {
+    document.getElementById("projet5").style.display = "block";
+}
+  
+function closeProjet5() {
+    document.getElementById("projet5").style.display = "none";
+}
+function projet6() {
+    document.getElementById("projet6").style.display = "block";
+}
+  
+function closeProjet6() {
+    document.getElementById("projet6").style.display = "none";
+}
+function proto() {
+    document.getElementById("proto").style.display = "block";
+}
+  
+function closeProto() {
+    document.getElementById("proto").style.display = "none";
+}
+
